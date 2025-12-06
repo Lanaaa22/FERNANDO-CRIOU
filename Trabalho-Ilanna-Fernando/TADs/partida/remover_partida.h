8b036e72aed865265ef38d61eb96ea18c4c82595
Fernando_Montenegro <158318208+Fernandinho2024@users.noreply.github.com>
2025-12-06 19:06:06 -0300

    Delete Trabalho-Ilanna-Fernando directory

--- Trabalho-Ilanna-Fernando/TADs/partida/remover_partida.h
+++
@@ -1,9 +0,0 @@
-#ifndef REMOVER_PARTIDA_H
-#define REMOVER_PARTIDA_H
-
-#include "../time/time.h"
-#include "../partida/partida.h"
-
-void RemoverPartida (bdTimes *bdt, bdPartidas *bdp);
-void RemoverPartidaArquivo(int id);
-#endif
