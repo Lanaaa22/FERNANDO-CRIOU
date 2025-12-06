61a51f911f103d161be94d0eed44f7ab8ab52153
Fernando_Montenegro <158318208+Fernandinho2024@users.noreply.github.com>
2025-12-06 19:05:49 -0300

    Delete FERNANDO-CRIOU-main directory

--- FERNANDO-CRIOU-main/README.md
+++
@@ -1,2 +0,0 @@
-# FERNANDO-CRIOU
-Trabalho EstDad
--- FERNANDO-CRIOU-main/Trabalho-Ilanna-Fernando/TADs/partida/atualizar_partida.h
+++
@@ -1,9 +0,0 @@
-#ifndef ATUALIZAR_PARTIDA_H
-#define ATUALIZAR_PARTIDA_H
-
-#include "../time/time.h"
-#include "../partida/partida.h"
-
-void printBDPartidas(bdPartidas *bd);
-
-#endif
